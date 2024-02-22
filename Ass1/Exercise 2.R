7edb4ac207bb7dce702d381b0c8337911d3053e0
ChackMociute <martynasvaznonis@gmail.com>
2024-02-22 17:03:33 +0100

    Finished the code

---
+++ Ass1/Exercise 2.R
@@ -0,0 +1,27 @@
+data <- read.table("hemoglobin-1.txt", header=T)
+data$rate <- as.factor(data$rate)
+data$method <- as.factor(data$method)
+
+# a)
+I=4; J=2; N=10
+data.frame(fish=sample(1:(N*I*J)), rate=rep(1:I, each=N*J), method=rep(1:J, N*I))
+
+
+# b)
+anova(lm(hemoglobin~rate*method, data=data))
+
+
+# c)
+anova(lm(hemoglobin~rate+method, data=data))
+summary(lm(hemoglobin~rate+method, data=data))
+
+mean(data$hemoglobin[data$rate==3])
+which.max(aggregate(hemoglobin ~ rate, data=data, mean)$hemoglobin)
+
+
+# d)
+anova(lm(hemoglobin ~ rate, data=data))
+
+
+# e)
+kruskal.test(hemoglobin ~ rate, data=data)
---
+++ Ass1/Exercise 3.R
@@ -0,0 +1,30 @@
+library(lme4)
+
+data <- read.table("cream-1.txt", header=T)
+data$batch <- as.factor(data$batch)
+data$position <- as.factor(data$position)
+data$starter <- as.factor(data$starter)
+
+# a)
+aciditylm = lm(acidity ~ batch + position + starter, data)
+anova(aciditylm)
+summary(aciditylm)
+
+
+# b)
+aciditylm = lm(acidity ~ batch + starter, data)
+anova(aciditylm)
+summary(aciditylm)
+
+
+# c)
+friedman.test(acidity ~ starter | batch, data)
+
+
+# d)
+lm1 = lmer(acidity ~ starter + (1|batch) + (1|position), data)
+lm2 = lmer(acidity ~ (1|batch) + (1|position), data)
+anova(lm1, lm2)
+
+
+
