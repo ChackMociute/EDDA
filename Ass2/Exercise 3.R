55df289ec3c60510a74d3e7e14e55dd7930396fe
ChackMociute <martynasvaznonis@gmail.com>
2024-03-08 10:51:06 +0100

    Finished assignment 2

--- Ass2/Exercise 3.R
+++ Ass2/Exercise 3.R
@@ -1,12 +1,24 @@
 data <- read.table("awards.txt", header=TRUE)
 data$prog <- as.factor(data$prog)
 
+
 # a)
 poisson.model = glm(num_awards ~ prog, data=data, family=poisson)
 summary(poisson.model)
 
+# Average number of awards per program
+aggregate(num_awards ~ prog, data=data, mean)
 
 
+# b)
+kruskal.test(num_awards ~ prog, data=data)
 
 
+# c)
+poisson.full = glm(num_awards ~ prog*math, data=data, family=poisson)
+poisson.nint = glm(num_awards ~ prog + math, data=data, family=poisson)
+summary(poisson.full)
+summary(poisson.nint)
 
+newdata = data.frame(math=56, prog=factor(1:3))
+predict(poisson.nint, newdata=newdata, type="response")
